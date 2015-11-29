bbe1f7096442488d924a9eee324c02fc4ffff016
Your Name <you@example.com>
2015-11-29 21:53:13 +0800

    头条新闻显示

--- src/cui/litang/cuinews/home/news/newstab/NewsTabPager.java
+++ src/cui/litang/cuinews/home/news/newstab/NewsTabPager.java
@@ -11,14 +11,17 @@ import com.lidroid.xutils.http.ResponseInfo;
 import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 import com.lidroid.xutils.view.annotation.ViewInject;
+import com.viewpagerindicator.CirclePageIndicator;
 
 import android.app.Activity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
+import android.widget.TextView;
 import android.widget.Toast;
 import cui.litang.cuinews.R;
 import cui.litang.cuinews.domain.NewsData.NewsChildData;
@@ -33,11 +36,17 @@ public class NewsTabPager extends BaseNewsMenuPager {
 	NewsChildData mChildData;
 	private String mUrl;
 	TabData mTabDetailData;
-	private ArrayList<TabNewsData> mNewsList; // 新闻数据集合
+	private ArrayList<TabNewsData> mTabLVNewsList; // 新闻数据集合
 	private ArrayList<TopNewsData> mTopNewsList;// 头条新闻数据集合
 	
 	@ViewInject(R.id.vp_news)
 	private ViewPager mViewPager;
+	
+	@ViewInject(R.id.tv_top_news_title)
+	private TextView tv_top_news_title;
+	
+	@ViewInject(R.id.cricle_indicator)
+	private CirclePageIndicator mCirclePageIndicator;
 
 	
 	
@@ -99,12 +108,43 @@ public class NewsTabPager extends BaseNewsMenuPager {
 		
 		mTopNewsList = mTabDetailData.data.topnews;
 
-		mNewsList = mTabDetailData.data.news;
+		mTabLVNewsList = mTabDetailData.data.news;
 		
 		if (mTopNewsList != null) {
 			
 			mViewPager.setAdapter(new TopNewsAdapter());
+			
+			tv_top_news_title.setText(mTopNewsList.get(0).title);
+
+			mViewPager.setOnPageChangeListener(new OnPageChangeListener(){
+
+				@Override
+				public void onPageScrolled(int position, float positionOffset,
+						int positionOffsetPixels) {
+					//  Auto-generated method stub
+					
+				}
+
+				@Override
+				public void onPageSelected(int position) {
+					tv_top_news_title.setText(mTopNewsList.get(position).title);
+				}
+
+				@Override
+				public void onPageScrollStateChanged(int state) {
+					//  Auto-generated method stub
+					
+				}
+				
+			});
+			
+			mCirclePageIndicator.setViewPager(mViewPager);
+			mCirclePageIndicator.setSnap(true);
+			mCirclePageIndicator.onPageSelected(0);
+			
 		}
+		
+		
 		
 	}
 	
@@ -140,7 +180,6 @@ public class NewsTabPager extends BaseNewsMenuPager {
 
 			TopNewsData topNewsData = mTopNewsList.get(position);
 			utils.display(image, topNewsData.topimage);// 传递imagView对象和图片地址
-
 			container.addView(image);
 			
 			System.out.println("instantiateItem....." + position);
@@ -152,5 +191,7 @@ public class NewsTabPager extends BaseNewsMenuPager {
 			container.removeView((View) object);
 		}
 	}
+	
+	
 
 }
